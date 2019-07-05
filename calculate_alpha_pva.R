a0c8e3ef29d3a1a813b0ba1ec4a46fbb481b7758
Unknown <njaya.iyer@gmail.com>
2019-07-04 21:02:08 -0700

    Updates to calculate alpha value
    - created new plot
    - new csv file

--- calculate_alpha_pva.R
+++ calculate_alpha_pva.R
@@ -1,71 +1,63 @@
-## Select the correct folder for either WLG or MTN data
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_IBM_50year_mtn_0.99/")
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_IBM_50year_mtn_0.85/")
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_IBM_50year_mtn_0.65/")
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_IBM_50year_wlg_0.42/")
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_LM_50year_mtn_3%/")
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_LM_50year_mtn_2%/")
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_LM_50year_mtn_1%/")
-##workingDir <- ("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/pva_LM_50year_wlg/")
-
-setwd(workingDir)
-allScenarioFiles <- list.files(pattern="*.csv")
-for (i in 1:length(allScenarioFiles)){
-  assign(allScenarioFiles[i], 
-         read.csv(paste(workingDir, allScenarioFiles[i], sep=''), header=TRUE)
-  )}
-
-########## IMPORTANT !!!!!! #########
-########## IMPORTANT !!!!!! #########
-########## IMPORTANT !!!!!! #########
-## Select simulation objects from list. Uncommented depending on whether IBM or LM:
-stochObjects <- c("IBM_Scenario1.csv","IBM_Scenario2.csv","IBM_Scenario3.csv","IBM_Scenario4.csv","IBM_Scenario5.csv","IBM_Scenario6.csv","IBM_Scenario7.csv","IBM_Scenario8.csv","IBM_Scenario9.csv")
-##stochObjects <- c("LM_Scenario1.csv","LM_Scenario2.csv","LM_Scenario3.csv","LM_Scenario4.csv","LM_Scenario5.csv","LM_Scenario6.csv","LM_Scenario7.csv","LM_Scenario8.csv","LM_Scenario9.csv")
-
-Nfinal <- data.frame(matrix(ncol=9, nrow=1000))
-colnames(Nfinal) <- LETTERS[1:9]
-
-index <- 0
-for(j in 1:length(stochObjects)){
-  index<-index+1
-  resX <- get(stochObjects[j])
-  Nfinal[,j] <- as.numeric(resX[480,])
-}
-
-##write.csv(Nfinal, file=paste0(workingDir_Results,"N40_mtn0.99_IBM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"N40_mtn0.85_IBM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"N40_mtn0.65_IBM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"Nfinal_mtn0.85_IBM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"Nfinal_mtn0.65_IBM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"Nfinal_wlg0.42_IBM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"Nfinal_mtn3%_LM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"Nfinal_mtn2%_LM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"Nfinal_mtn1%_LM.csv"), row.names=F)
-##write.csv(Nfinal, file=paste0(workingDir_Results,"Nfinal_wlg_LM.csv"), row.names=F)
-
 #######################################################
 ############### DETERMINE CORRECT alpha ###############
 #######################################################
 
-workingDir <- "~/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/"
-setwd(workingDir)
+## Make sure your current working directory is the main folder:
+setwd("/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/")
+
+## If these directories do not exsit, run this:
+dir.create("PVA_Output")
+dir.create("PVA_Input")
+dir.create("PVA_Output/Results")
+dir.create("PVA_Output/LM_Projection")
+dir.create("PVA_Output/IBM_Projection")
+
+## Select the working directory for input files:
+workingDir_Input <- "/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/PVA_Input/"
+
+## Select the working directory for output files:
+workingDir_Output <- "/Users/neethaiyer/Desktop/PVA_Tshiaberimu_R/PVA_Output/"
+
+## DAMIEN !!!!! Your wd: ## Make sure to update as above!
+## workingDir <- "~/Documents/git repositories/PVA_Tshiaberimu_R/"
+
+## Source the functions used in simulations below:
 source("1. Function Definitions.R") 
 
-#### Rerun deterministic projection with No=30, t= 50 years
-## Reintroduction Scenarios:
-ReintroScenario <- read.csv(paste0(workingDir, "ReintroductionScenarios_LM.csv")) ## csv file with Reintroduction Scenarios for LM
+####################################################################################
+############################ READ LIFE HISTORY TABLE ###############################
+####################################################################################
+setwd(workingDir_Input)
 
-mat <- as.matrix(read.csv(paste0(workingDir, "LeslieMatrix_MTN_3%.csv"))) ## csv file with appropriate Leslie Matrix (needs to be converted to matrix object!)
-
-dat <- read.csv(paste0(workingDir, "Gorilla_LifeTables.csv"))
+## Your life history tables should have at least 3 columns: age, mortality rate, and fertility rate:
+dat <- read.csv("Gorilla_LifeTables.csv")
 dat$fertilityrate_2percent <- dat[,3]*.786 
 ## fertility rates multiplied by factor less than 1 to get eigen values of 1.01 which corresponds to a 1% growth rate
 dat$fertilityrate_1percent <- dat[,3]*.643 
 ## fertility rates multiplied by factor less than 1 to get eigen values of 1.02 which corresponds to a 2% growth rate
 
+##################################################################
+############## Create an object that selects the LM ##############
+##################################################################
+
+selectLM <- read.csv("LeslieMatrix_MTN_2%.csv")
+
+###############################################################################
+############## SET THE INITIAL CONDITIONS OF THE LM & IBM MODELS ##############
+###############################################################################
+
+## Reintroduction Scenarios:
+ReintroScenario <- read.csv("ReintroductionScenarios_LM.csv") ## csv file with Reintroduction Scenarios for LM
+
+## Leslie Matrix parameters:
+mat <- as.matrix(selectLM) ## LM needs to be converted to matrix object!
+
+## Time parameters:
 nyears <- 50 ## Projection Period
 nruns <- 1000 ## Number of simulations to run
+timeunit <- 1/12 ## timestep for IBM
 
+## Initial demographic parameters: survivorship, fertility, and weaning age
 datX <- dat[,1:3] ## Subset appropriate life history columns: dat[,c(1,4:5)] for WLG, dat[,1:3] for MTN
 ## NOTE: this subsetting is needed because columns for dat are specified in FUNCTIONS 8 and 9
 weaningAge <- 3.5 ## 4.5 for WLG, 3.5 for MTN
@@ -75,7 +67,7 @@ temp <- pop_projection(tfinal=nyears, LM=mat, No=ReintroScenario[,11]) ## run th
 Nfinal <- data.frame(ReintroScenario$age, temp[,51])
 colnames(Nfinal) <- c("age","N")
 sum(Nfinal$N) ## check if Nfinal is about the same as Nfinal_expected for the required lambda value
-Nfinal_expected <- 1.032^50*30 ## Nfinal_expected = lambda^t*No
+Nfinal_expected <- 1.032^50*30 ## Nfinal_expected = lambda^t*No (lambda for 3% = 1.032)
 N_random <- rep(Nfinal$age, round(Nfinal$N)) ## create vector with individuals and their ages
 N_random <- sample(N_random, size=100, replace = FALSE, prob = NULL) ## randomly sample 100 individuals in each age class
 N_random <- data.frame(subset(N_random, N_random>weaningAge)) ## remove individuals younger than weaning age
@@ -88,7 +80,7 @@ growthRates <- data.frame(alpha_value = NA,
 timeunit<-1/12
 initalConditions <- convertToList(scenario = N_random, adultAge=adultAge, weaningAge=weaningAge) ## define initial conditions based on ages of females randomly sampled earlier in N_random
 nruns <- 10
-alpha <- 0.35 ## set alpha value
+alpha <- 0.75 ## set alpha value
 
 res <- matrix(0, nrow=trunc(nyears/timeunit)+1, ncol=nruns)
 for(j in 1:length(initalConditions)){
@@ -98,19 +90,29 @@ for(j in 1:length(initalConditions)){
     nindiv <- tapply(abmDataLog$status,abmDataLog$timestep, function(v) length(v)+rbinom(1, sum(v=="L"), .5))##we're adding the unweaned females
     res[1:length(nindiv),i] <- nindiv
   }
-  ##write.csv(res, file=paste0("pva_projection_IBM/IBM_Scenario", j,".csv"), row.names=F)
 }
 
 finalPop <- as.numeric(res[nrow(res),])
 startPop <- as.numeric(res[480,])
-##startPop <- rep(100,nruns)
+##startPop <- rep(100,nruns) ## if using No at t=0
 
 logLambda <- mean((1/10)*log(finalPop/startPop)) ## nyears for the census time period, loglambda = 1/timeperiod*log(Ntfinal)/Nt0
 lambda <- exp(logLambda)
-growthRates[17,1:3] <- c(alpha, round(logLambda, digits=3), round(lambda, digits=3))
+growthRates[13,1:3] <- c(alpha, round(logLambda, digits=3), round(lambda, digits=3))
 growthRates <- growthRates[order(-growthRates$alpha_value),] 
 growthRates
 
-write.csv(growthRates, file=paste0("pva_projection_IBM/growthRates.csv"), row.names=F)
-## change row to 15 for next alpha
+write.csv(growthRates, file="growthRates_LM1%.csv", row.names=F)
+
+## Examine how alpha and growth rate vary:
+growthRates2 <- read.csv("growthRates_LM2%.csv")
+growthRates3 <- read.csv("growthRates_LM3%.csv")
+
+plot(growthRates2[,1:2])
+points(growthRates3[,1:2], col=2)
+abline(lm(growthRates2[,2]~growthRates2[,1]))
+abline(lm(growthRates3[,2]~growthRates3[,1]), col=2)
+
+
+
 
